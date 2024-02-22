38da37e28e9aeef0ca23642b2898a8a78263be61
Kamila Ingrid de Carvalho <50252782879@>
2024-02-22 11:14:38 -0300

    test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os
 from http.server import SimpleHTTPRequestHandler
 import socketserver
 from urllib.parse import parse_qs, urlparse
+import hashlib
 
 
 class MyHandler(SimpleHTTPRequestHandler):
@@ -79,6 +80,62 @@ class MyHandler(SimpleHTTPRequestHandler):
 
             # evita a execução do resto
             return
+        
+        elif self.path.startswith('/turmas'):
+            query_params = parse_qs(urlparse(self.path).query)
+            code = query_params.get('code', [' '])[0]
+            descricao = query_params.get('descricao', [' '])[0]
+
+            # msg boas vindas
+            message = f"Olá, vamos cadastrar sua turma nova!"
+
+            # responde com a page de cadastro
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+
+            # le o conteudo da page cadastro
+            with open(os.path.join(os.getcwd(), 'turmas.html'), 'r', encoding='utf-8') as turmas_file:
+                content = turmas_file.read()
+
+            # substitui os marcadores de posição pelos valores correspondentes
+            content = content.replace('{code}', code)
+            content = content.replace('{descricao}', descricao)
+            content = content.replace('{message}', message)
+
+            # envia para o cliente
+            self.wfile.write(content.encode('utf-8'))
+
+            # evita a execução do resto
+            return
+        
+        elif self.path.startswith('/atividades'):
+            query_params = parse_qs(urlparse(self.path).query)
+            codigo = query_params.get('codigo', [' '])[0]
+            descricao = query_params.get('descricao', [' '])[0]
+
+            # msg boas vindas
+            message = f"Olá, vamos cadastrar sua atividade nova!"
+
+            # responde com a page de cadastro
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+
+            # le o conteudo da page cadastro
+            with open(os.path.join(os.getcwd(), 'atividades.html'), 'r', encoding='utf-8') as atividades_file:
+                content = atividades_file.read()
+
+            # substitui os marcadores de posição pelos valores correspondentes
+            content = content.replace('{codigo}', codigo)
+            content = content.replace('{descricao}', descricao)
+            content = content.replace('{message}', message)
+
+            # envia para o cliente
+            self.wfile.write(content.encode('utf-8'))
+
+            # evita a execução do resto
+            return
 
         else:
             # se não achar a rota "/login", continua o comportamento padrão
@@ -89,13 +146,52 @@ class MyHandler(SimpleHTTPRequestHandler):
         with open('dados_login.txt', 'r', encoding='utf-8') as file:
             for line in file:
                 if line.strip():
-                    stored_login, stored_senha, stored_nome = line.strip().split(';')
+                    stored_login, stored_senha_hash, stored_nome = line.strip().split(';')
                     if login == stored_login:
                         print("Cheguei aqui significando que localizei o login informado.")
                         print("Senha:" + senha)
                         print("Senha armazenada:" + senha)
-                        return senha == stored_senha
+
+                        senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+                        return senha_hash == stored_senha_hash
         return False
+    
+    def turma_existente(self, code, descricao):
+        # Verifica a existência da turma
+        with open('dados_turmas.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                if line.strip():
+                    code_line, descricao_line = line.strip().split(';')
+                    if code == code_line and descricao == descricao_line:
+                        print("Código: ", code)
+                        return True
+        return False
+    
+    def atividade_existente(self, codigo, descricao):
+        # Verifica a existência da turma
+        with open('dados_atividades.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                if line.strip():
+                    codigo_line, descricao_line = line.strip().split(';')
+                    if codigo == codigo_line and descricao == descricao_line:
+                        print("Código: ", codigo)
+                        return True
+        return False
+
+    def adicionar_usuario(self, login, senha, nome):
+        senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        with open('dados_login.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{login};{senha_hash};{nome}\n')
+
+    def adicionar_turma(self, code, descricao):
+        # Adiciona uma nova turma ao arquivo
+        with open('dados_turmas.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{code};{descricao};\n')
+
+    def adicionar_turma(self, codigo, descricao):
+        # Adiciona uma nova turma ao arquivo
+        with open('dados_atividades.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{codigo};{descricao};\n')
 
     def remover_ultima_linha(self, arquivo):
         print("Vou excluir a última linha.")
@@ -143,8 +239,7 @@ class MyHandler(SimpleHTTPRequestHandler):
                     return  # adiciona para evitar rodar o resto do código
                 else:
                     # adc novo usuário
-                    with open('dados_login.txt', 'a', encoding='utf-8') as file:
-                        file.write(f"{login};{senha};" + "none\n")
+                    self.adicionar_usuario(login, senha, nome='None')
 
                     # redireciona
                     self.send_response(302)
@@ -166,6 +261,8 @@ class MyHandler(SimpleHTTPRequestHandler):
             senha = form_data.get('password', [''])[0]
             nome = form_data.get('nome', [''])[0]
 
+            senha_hash = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+
             print(f'Nome: {nome}')
 
             # verifica existencia do usuario
@@ -178,8 +275,8 @@ class MyHandler(SimpleHTTPRequestHandler):
                 with open('dados_login.txt', 'w', encoding='utf-8') as file:
                     for line in lines:
                         stored_login, stored_senha, stored_nome = line.strip().split(';')
-                        if login == stored_login and senha == stored_senha:
-                            line = f'{login};{senha};{nome}\n'
+                        if login == stored_login and senha_hash == stored_senha:
+                            line = f'{login};{senha_hash};{nome}\n'
                         file.write(line)
 
                 self.send_response(302)
@@ -187,12 +284,74 @@ class MyHandler(SimpleHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write("Registro recebido com sucesso!".encode('utf-8'))
 
+        elif self.path.startswith('/cad_turma'):
+            # Obtém o comprimento do corpo da requisição
+            content_length = int(self.headers['Content-Length'])
+            # Lê o corpo
+            body = self.rfile.read(content_length).decode('utf-8')
+            # Analisa os dados do formulário
+            form_data = parse_qs(body, keep_blank_values=True)
+
+            # Extrai os dados do formulário
+            code = form_data.get('code', [''])[0]
+            descricao = form_data.get('descricao', [''])[0]
+
+            print(f'Turma: {descricao}')
+
+            # Verifica a existência da turma
+            if self.turma_existente(code, descricao):
+                # Turma já existe, não é necessário fazer nada
+                self.send_response(200)
+                self.send_header("Content-type", "text/html; charset=utf-8")
+                self.end_headers()
+                self.wfile.write("Turma já existe!".encode('utf-8'))
+            else:
+                # Adiciona a nova turma ao arquivo
+                with open('dados_turmas.txt', 'a', encoding='utf-8') as file:
+                    file.write(f'{code};{descricao}\n')
+
+                self.send_response(302)
+                self.send_header("Content-type", "text/html; charset=utf-8")
+                self.end_headers()
+                self.wfile.write("Nova turma recebida com sucesso!".encode('utf-8'))
+
+        elif self.path.startswith('/cad_atividade'):
+            # Obtém o comprimento do corpo da requisição
+            content_length = int(self.headers['Content-Length'])
+            # Lê o corpo
+            body = self.rfile.read(content_length).decode('utf-8')
+            # Analisa os dados do formulário
+            form_data = parse_qs(body, keep_blank_values=True)
+
+            # Extrai os dados do formulário
+            codigo = form_data.get('codigo', [''])[0]
+            descricao = form_data.get('descricao', [''])[0]
+
+            print(f'Atividade: {descricao}')
+
+            # Verifica a existência da turma
+            if self.atividade_existente(codigo, descricao):
+                # atividade já existe, não é necessário fazer nada
+                self.send_response(200)
+                self.send_header("Content-type", "text/html; charset=utf-8")
+                self.end_headers()
+                self.wfile.write("Atividade já existe!".encode('utf-8'))
+            else:
+                # Adiciona a nova turma ao arquivo
+                with open('dados_atividades.txt', 'a', encoding='utf-8') as file:
+                    file.write(f'{codigo};{descricao}\n')
+
+                self.send_response(302)
+                self.send_header("Content-type", "text/html; charset=utf-8")
+                self.end_headers()
+                self.wfile.write("Nova atividade recebida com sucesso!".encode('utf-8'))
+
         else:
             self.remover_ultima_linha('dados_login.txt')
             self.send_response(302)
             self.send_header("Content-type", "text/html; charset=utf-8")
             self.end_headers()
-            self.wfile.write("RSenha não confere. Retome o procedimento.".encode('utf-8'))
+            self.wfile.write("Senha não confere. Retome o procedimento.".encode('utf-8'))
 
 
 # define a porta a ser utilizada
